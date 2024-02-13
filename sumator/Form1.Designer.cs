35953a3359f75593d7b268a04d8dbe1f353362b3
AlexeiKonev <alexkonev777@gmail.com>
2024-02-13 14:36:33 +0300

    склеивания текста из разных файлов в один

--- sumator/Form1.Designer.cs
+++ sumator/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +13,64 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCombine = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLinesPerFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+
+            // btnCombine
+            this.btnCombine.Location = new System.Drawing.Point(35, 30);
+            this.btnCombine.Name = "btnCombine";
+            this.btnCombine.Size = new System.Drawing.Size(120, 40);
+            this.btnCombine.Text = "Combine Files";
+            this.btnCombine.UseVisualStyleBackColor = true;
+            this.btnCombine.Click += new System.EventHandler(this.btnCombine_Click);
+
+            // btnSplit
+            this.btnSplit.Location = new System.Drawing.Point(35, 90);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(120, 40);
+            this.btnSplit.Text = "Split File";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.Text = "Lines Per File:";
+
+            // txtLinesPerFile
+            this.txtLinesPerFile.Location = new System.Drawing.Point(155, 160);
+            this.txtLinesPerFile.Name = "txtLinesPerFile";
+            this.txtLinesPerFile.Size = new System.Drawing.Size(60, 26);
+            this.txtLinesPerFile.Text = "1000";
+
+            // MainForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(300, 250);
+            this.Controls.Add(this.btnCombine);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLinesPerFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text File Manipulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.Button btnCombine;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLinesPerFile;
     }
 }
 
--- sumator/Form1.cs
+++ sumator/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,83 @@ namespace sumator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private void btnCombine_Click(object sender, EventArgs e)
+        {
+            CombineFiles();
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            SplitFile();
+        }
+
+        private void CombineFiles()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text Files|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] fileNames = openFileDialog.FileNames;
+                string combinedText = "";
+
+                foreach (string fileName in fileNames)
+                {
+                    combinedText += File.ReadAllText(fileName);
+                }
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text File|*.txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string saveFileName = saveFileDialog.FileName;
+                    File.WriteAllText(saveFileName, combinedText);
+                    MessageBox.Show("Files combined successfully!");
+                }
+            }
+        }
+
+        private void SplitFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text File|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                string[] lines = File.ReadAllLines(fileName);
+
+                int linesPerFile = Convert.ToInt32(txtLinesPerFile.Text);
+
+                List<string> currentLines = new List<string>();
+                int fileNumber = 1;
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    currentLines.Add(lines[i]);
+
+                    if (currentLines.Count == linesPerFile || i == lines.Length - 1)
+                    {
+                        string outputFileName = $"output_{fileNumber}.txt";
+                        string outputPath = Path.Combine(Path.GetDirectoryName(fileName), outputFileName);
+
+                        File.WriteAllLines(outputPath, currentLines.ToArray());
+                        currentLines.Clear();
+                        fileNumber++;
+                    }
+                }
+
+                MessageBox.Show("File split successfully!");
+            }
         }
     }
 }
